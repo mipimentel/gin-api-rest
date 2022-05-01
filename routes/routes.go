714b765ea7039f1b2d6e1976fe0225b14b6344e1
Marcelo Pimentel <marcelo.ide.pimentel@gmail.com>
2022-05-01 14:57:47 -0300

    Adicionada pagina index listando nomes de alunos e pagina 404

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,8 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
@@ -14,5 +16,7 @@ func HandleRequests() {
 	r.PATCH("/alunos/:id", controllers.EditaAluno)
 	r.GET("alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.GET(":nome", controllers.Saudacao)
+	r.GET("/index", controllers.ExibePaginaIndex)
+	r.NoRoute(controllers.RotaNaoEncontrada)
 	r.Run()
 }
